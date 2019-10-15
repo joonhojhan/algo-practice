4a1d113cae18de440acfd10eca66ce1566091208
Joonho Han <joonhojhan@gmail.com>
2019-10-15 13:54:22 -0400

    adds find_duplicate implementation for finding duplicate number in an array of n+1 integers

---
+++ find_duplicate.js
@@ -0,0 +1,19 @@
+const findDuplicate = nums => {
+	let h = nums[0],
+		t = nums[0];
+	do {
+		t = nums[t];
+		h = nums[h];
+		h = nums[h];
+	} while (h !== t);
+	h = nums[0];
+	while (h !== t) {
+		t = nums[t];
+		h = nums[h];
+	}
+	return h;
+};
+
+console.log(findDuplicate([2, 5, 9, 6, 9, 3, 8, 9, 7, 1]) === 9);
+console.log(findDuplicate([1, 3, 4, 2, 2]) === 2);
+console.log(findDuplicate([3, 1, 3, 4, 2]) === 3);
