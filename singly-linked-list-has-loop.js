795581a4d9fa90e8b44f735eb4e45817c87b7565
Joonho Han <joonhojhan@gmail.com>
2019-10-16 18:59:07 -0400

    adds reversing a singly linked list and loop detection of a singly linked list with constant space

---
+++ singly-linked-list-has-loop.js
@@ -0,0 +1,17 @@
+/*
+Given a linked list, determine if it has a cycle in it.
+
+To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
+*/
+
+var hasCycle = function(head) {
+	let t = head,
+		h = head;
+	do {
+		if (t === null || h === null || t.next === null || h.next === null)
+			return false;
+		t = t.next;
+		h = h.next.next;
+	} while (t !== h);
+	return true;
+};
