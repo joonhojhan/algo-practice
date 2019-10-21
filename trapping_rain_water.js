b8ad7c5e42f5e822d4b1d8d7da04d25a38a938db
Joonho Han <joonhojhan@gmail.com>
2019-10-21 15:04:59 -0400

    adds trapping rain water implementation

---
+++ trapping_rain_water.js
@@ -0,0 +1,39 @@
+/*
+Given n non-negative integers representing an elevation map where the width
+of each bar is 1, compute how much water it is able to trap after raining.
+*/
+
+const trap = height => {
+	// initialize pointers for left and right idx, total trapped water, and max left and right heights
+	let left = 0,
+		right = height.length - 1,
+		leftMax = 0,
+		rightMax = 0,
+		total = 0;
+	// while left idx is less than right idx
+	while (left < right) {
+		// if left height is less than right height
+		if (height[left] < height[right]) {
+			// set leftMax to left height if left height is greater than the current left max, else add to total trapped water
+			height[left] >= leftMax
+				? (leftMax = height[left])
+				: (total += leftMax - height[left]);
+			// increment left idx
+			left++;
+			// if right height is less than left height
+		} else {
+			// set right max to right height if right height is greater than the current right max, else add to total trapped water
+			height[right] >= rightMax
+				? (rightMax = height[right])
+				: (total += rightMax - height[right]);
+			// decrement right idx
+			right--;
+		}
+	}
+	// return total trapped water
+	return total;
+};
+
+console.log(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]) === 6);
+console.log(trap([0, 1, 0, 2, 1, 0, 1, 3, 4, 1, 0, 0]));
+console.log(trap([5, 1, 7, 0, 1, 10, 0, 3, 6, 1, 2, 5]));
