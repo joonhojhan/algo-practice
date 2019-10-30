c11198206fa92b007385cc49a89057ab11b0922f
Joonho Han <joonhojhan@gmail.com>
2019-10-30 15:39:05 -0400

    adds permutations and top k frequent elements implementations

---
+++ permutations.js
@@ -0,0 +1,23 @@
+/*
+Given a collection of distinct integers, return all possible permutations.
+*/
+const permute = nums => {
+	let results = [[nums.shift()]];
+	while (nums.length) {
+		let nextEl = nums.shift();
+		let currentResults = [];
+		for (let i = 0; i < results.length; i++) {
+			for (let j = 0; j <= results[i].length; j++) {
+				currentResults.push([
+					...results[i].slice(0, j),
+					nextEl,
+					...results[i].slice(j),
+				]);
+			}
+		}
+		results = currentResults;
+	}
+	return results;
+};
+
+console.log(permute([1, 2, 3]));
---
+++ top_k_frequent_elements.js
@@ -0,0 +1,30 @@
+/*
+Given a non-empty array of integers, return the k most frequent elements.
+*/
+const topKFrequent = function(nums, k) {
+	const freq = {};
+	for (let num of nums) {
+		if (freq[num]) freq[num]++;
+		else freq[num] = 1;
+	}
+	let results = [];
+	while (results.length < k) {
+		let mostFreq = null;
+		let compare = null;
+		for (let el in freq) {
+			if (freq[el] > compare) {
+				compare = freq[el];
+				mostFreq = el;
+			}
+		}
+		delete freq[mostFreq];
+		results.push(mostFreq);
+	}
+	return results;
+};
+
+console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2));
+console.log(topKFrequent([3, 0, 1, 0], 1));
+console.log(
+	topKFrequent([1, 2, 2, 7, 8, 9, 3, 5, 5, 3, 2, 4, 6, 7, 1, 5, 5, 5, 5, 5], 3)
+);
