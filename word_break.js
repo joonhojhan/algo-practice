aba4146a9b82f874e4aa89cbb4f8609e3abda017
Joonho Han <joonhojhan@gmail.com>
2019-11-22 12:27:43 -0500

    adds word break implementation

---
+++ word_break.js
@@ -0,0 +1,43 @@
+/*
+Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+Note:
+
+The same word in the dictionary may be reused multiple times in the segmentation.
+You may assume the dictionary does not contain duplicate words.
+Example 1:
+
+Input: s = "leetcode", wordDict = ["leet", "code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+Example 2:
+
+Input: s = "applepenapple", wordDict = ["apple", "pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+             Note that you are allowed to reuse a dictionary word.
+Example 3:
+
+Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+Output: false
+*/
+
+const wordBreak = function(s, wordDict) {
+	let dict = new Set(wordDict);
+	let dp = new Array(s.length + 1).fill(false);
+	dp[0] = true;
+	for (let i = 0; i < s.length; i++) {
+		for (let j = i; j >= 0; j--) {
+			let isTrue = dp[j];
+			let word = s.slice(j, i + 1);
+			if (isTrue && dict.has(word)) {
+				dp[i + 1] = true;
+			}
+		}
+	}
+	return dp[s.length];
+};
+
+console.log(wordBreak('leetcode', ['leet', 'code']) === true);
+console.log(wordBreak('applepenapple', ['apple', 'pen']) === true);
+console.log(wordBreak('catsandog', ['cats', 'dog', 'sand', 'and', 'cat']) === false);
