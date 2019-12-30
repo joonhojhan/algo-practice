db71ef96518f1c79705f3f89ee4f0652985020c2
Joonho Han <joonhojhan@gmail.com>
2019-12-29 19:30:26 -0500

    adds implementation for merging two sorted linked lists

---
+++ merge_two_sorted_lists.js
@@ -0,0 +1,38 @@
+/*
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+*/
+
+function ListNode(val) {
+	this.val = val;
+	this.next = null;
+}
+
+const mergeTwoLists = function(l1, l2) {
+	let curr1 = l1,
+		curr2 = l2;
+	if (!l1 && !l2) return null;
+	if (!l1) return l2;
+	if (!l2) return l1;
+	let res =
+		curr1.val > curr2.val ? new ListNode(curr2.val) : new ListNode(curr1.val);
+	curr1.val > curr2.val ? (curr2 = curr2.next) : (curr1 = curr1.next);
+	let currRes = res;
+	while (curr1 && curr2) {
+		if (curr1.val > curr2.val) {
+			currRes.next = curr2;
+			curr2 = curr2.next;
+			currRes = currRes.next;
+		} else {
+			currRes.next = curr1;
+			curr1 = curr1.next;
+			currRes = currRes.next;
+		}
+	}
+	curr1 ? (currRes.next = curr1) : (currRes.next = curr2);
+	return res;
+};
