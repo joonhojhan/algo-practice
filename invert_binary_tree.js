6567ea58b6c7175c51c6ffc0d7e7e0c60df429f1
Joonho Han <joonhojhan@gmail.com>
2020-01-16 18:18:23 -0500

    adds invert binary tree implementation

---
+++ invert_binary_tree.js
@@ -0,0 +1,30 @@
+/*
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+*/
+
+const invertTree = function(root) {
+	if (!root) return null;
+	let temp = root.left;
+	root.left = root.right;
+	root.right = temp;
+	if (root.left) invertTree(root.left);
+	if (root.right) invertTree(root.right);
+	return root;
+};
