ef6469cc30400d02a34aeb6484542e24df23d6e5
Joonho Han <joonhojhan@gmail.com>
2020-01-31 17:26:30 -0500

    adds KMP algorithm for finding substring in a string implementation

---
+++ knuth_morris_pratt.js
@@ -0,0 +1,43 @@
+const buildLPS = function(patt) {
+	let i = 0;
+	let j = 1;
+	let res = Array(patt.length).fill(0);
+	while (j < patt.length) {
+		if (patt[i] === patt[j]) {
+			res[j] = i + 1;
+			i++;
+			j++;
+		} else if (i > 0) {
+			i = res[i - 1];
+		} else {
+			res[j] = 0;
+			j++;
+		}
+	}
+	return res;
+};
+
+const find = function(str, patt) {
+	let lps = buildLPS(patt);
+	let res = [];
+	let i = 0;
+	let j = 0;
+	while (i < str.length) {
+		if (str[i] === patt[j]) {
+			i++;
+			j++;
+		}
+		if (j === patt.length) {
+			res.push(i - j);
+			j = lps[j - 1];
+		} else if (i < str.length && str[i] !== patt[j]) {
+			if (j > 0) j = lps[j - 1];
+			else i++;
+		}
+	}
+	return res;
+};
+
+console.log(find('adsgwadsxdsgwadsgz', 'dsgwadsgz'));
+console.log(find('aaabaacaaaefs', 'aaa'));
+console.log(find('abxabcabcaby', 'abcaby'));
