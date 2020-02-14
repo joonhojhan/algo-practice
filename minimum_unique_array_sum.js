4128b1767d2debfde2ba67ddbdc9038bf4d48e8e
Joonho Han <joonhojhan@gmail.com>
2020-02-14 15:43:51 -0500

    adds minimum unique array sum implementation

---
+++ minimum_unique_array_sum.js
@@ -0,0 +1,29 @@
+/*
+Given an array, incremen any duplicate elements until all elements are unique.
+The sum of the elements must be the minimum possible.
+
+Example:
+arr = [3, 2, 1, 2, 7]
+returns 17
+*/
+
+function getMinimumUniqueSum(arr) {
+	arr.sort((a, b) => a - b);
+	let n = arr.length;
+	let sum = arr[0];
+	let previous = arr[0];
+	for (let i = 1; i < n; i++) {
+		if (arr[i] <= previous) {
+			previous++;
+			sum += previous;
+		} else {
+			sum += arr[i];
+			previous = arr[i];
+		}
+	}
+	return sum;
+}
+
+console.log(getMinimumUniqueSum([3, 2, 1, 2, 7]) === 17);
+console.log(getMinimumUniqueSum([1, 1, 2, 2, 3, 4, 10, 11]) === 42);
+console.log(getMinimumUniqueSum([1, 2, 2, 3, 3, 4]) === 21);
