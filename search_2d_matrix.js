f2dc88bbeba51062c71b6729128785d6262c8eb6
Joonho Han <joonhojhan@gmail.com>
2020-04-21 17:53:12 -0400

    optimizes search 2d matrix implementation

--- search_2d_matrix.js
+++ search_2d_matrix.js
@@ -24,25 +24,42 @@ matrix = [
 target = 13
 Output: false
 */
+
+// const searchMatrix = function (matrix, target) {
+// 	if (!matrix.length || !matrix[0].length) return false;
+// 	let top = 0;
+// 	let bottom = matrix.length - 1;
+// 	let left = 0;
+// 	let right = matrix[0].length - 1;
+// 	let row;
+// 	while (top <= bottom) {
+// 		row = Math.floor((top + bottom) / 2);
+// 		if (matrix[row][0] === target) return true;
+// 		else if (matrix[row][0] > target) bottom = row - 1;
+// 		else top = row + 1;
+// 	}
+// 	if (matrix[row][0] > target && row - 1 >= 0) row--;
+// 	while (left <= right) {
+// 		const mid = Math.floor((left + right) / 2);
+// 		if (matrix[row][mid] === target) return true;
+// 		else if (matrix[row][mid] > target) right = mid - 1;
+// 		else left = mid + 1;
+// 	}
+// 	return false;
+// };
+
+// Optimized
 const searchMatrix = function (matrix, target) {
 	if (!matrix.length || !matrix[0].length) return false;
-	let top = 0;
-	let bottom = matrix.length - 1;
-	let left = 0;
-	let right = matrix[0].length - 1;
-	let row;
-	while (top <= bottom) {
-		row = Math.floor((top + bottom) / 2);
-		if (matrix[row][0] === target) return true;
-		else if (matrix[row][0] > target) bottom = row - 1;
-		else top = row + 1;
-	}
-	if (matrix[row][0] > target && row - 1 >= 0) row--;
-	while (left <= right) {
-		const mid = Math.floor((left + right) / 2);
-		if (matrix[row][mid] === target) return true;
-		else if (matrix[row][mid] > target) right = mid - 1;
-		else left = mid + 1;
+	const n = matrix.length;
+	const m = matrix[0].length;
+	let r = 0;
+	let c = m - 1;
+	while (r < n && c >= 0) {
+		const curr = matrix[r][c];
+		if (curr === target) return true;
+		if (curr > target) c--;
+		else r++;
 	}
 	return false;
 };
